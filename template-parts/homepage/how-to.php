48a8c8985ea6231992431a7619ecde6707b5c022
Frank Ponte <frankponte95@gmail.com>
2021-09-06 14:10:47 -0400

    How to steps are now dynamic

--- template-parts/homepage/how-to.php
+++ template-parts/homepage/how-to.php
@@ -1,34 +1,33 @@
 <?php
-    /**
-     * How to buy a course section
-     * @author FrankPonte
-     */
-    $content = get_field("how_to_section");
-    $button = $content["button"];
+
+/**
+ * How to buy a course section
+ * @author FrankPonte
+ */
+$content = get_field("how_to_section");
+$button = $content["button"];
+$steps = $content["steps"];
 ?>
 
 <section id="how-to-buy-a-course" class="container d-flex flex-column align-items-center justify-content-center my-5 py-5 px-5 px-lg-0">
     <h2 class="text-center fw-bold mb-5">
         <?php echo $content["title"]; ?>
     </h2>
-    <div class="d-none d-lg-block my-5 w-100">
-        <?php theme_lazy_image(144,"w-100 h-auto d-none d-lg-block","",1589,354); ?>
-    </div>
-    <div class="d-block d-lg-none w-100 w-md-75 mx-auto my-4">
-        <div class="row gx-5 gy-4">
-            <div class="col-6">
-                <?php theme_lazy_image(151,"w-100 h-auto","",130,102); ?>
-            </div>
-            <div class="col-6">
-                <?php theme_lazy_image(150,"w-100 h-auto","",130,102); ?>
-            </div>
-            <div class="col-6">
-                <?php theme_lazy_image(149,"w-100 h-auto","",130,102); ?>
-            </div>
-            <div class="col-6">
-                <?php theme_lazy_image(152,"w-100 h-auto","",130,102); ?>
-            </div>
-        </div>
+    <div class="row w-100 mx-auto">
+        <?php
+        get_template_part("template-parts/homepage/how-to/steps", "How to section", [
+            "image" => $steps["step_1"]["image"],
+            "title" => $steps["step_1"]["title"]
+        ]);
+        get_template_part("template-parts/homepage/how-to/steps", "How to section", [
+            "image" => $steps["step_2"]["image"],
+            "title" => $steps["step_2"]["title"]
+        ]);
+        get_template_part("template-parts/homepage/how-to/steps", "How to section", [
+            "image" => $steps["step_3"]["image"],
+            "title" => $steps["step_3"]["title"]
+        ]);
+        ?>
     </div>
     <a href=" <?php echo esc_url($button["url"]); ?>" class="btn btn-primary mt-5">
         <?php esc_html_e($button["text"]); ?>
---
+++ template-parts/homepage/how-to/steps.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * How to buy a course section step
+ * @author FrankPonte
+ */
+?>
+
+<div class="col-12 col-md-4 text-center gy-5">
+    <?php theme_lazy_image($args["image"],"w-75 mx-auto"); ?>
+    <h5 class="fw-bold">
+        <?php esc_html_e($args["title"]); ?>
+    </h5>
+</div>
