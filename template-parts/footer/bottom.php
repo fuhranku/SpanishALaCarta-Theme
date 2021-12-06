2e5d4d60982fdc958e8e73869f6e60a29554f52c
Frank Ponte <frankponte95@gmail.com>
2021-12-05 20:21:46 -0400

    feature (bottom footer menu): added dynamic menu for policies

--- template-parts/footer/bottom.php
+++ template-parts/footer/bottom.php
@@ -5,12 +5,31 @@
  * 
  * @author FrankPonte
  */
+$menu_class = \SPANISHALACARTE\Inc\Menus::get_instance();
+$menu_id = $menu_class->get_menu_id('salc-bottom-footer-menu');
+$menu = wp_get_nav_menu_items($menu_id);
 ?>
 
 <div class="container-fluid bg-dark-blue d-flex flex-column justify-content-center align-items-center py-3">
     <p class="fw-bold fs-6 text-white text-center text-lg-start">Copyright 2021 © Spanisch a la carte</p>
-    <div class="d-flex flex-wrap flex-lg-nowrap d-flex-lg-inline justify-content-center">
-        <a href="#" class="fs-6 text-white me-2 me-lg-4 text-center text-lg-start my-3 my-md-0"><?php pll_e("Privacy Policy"); ?></a>
-        <a href="#" class="fs-6 text-white text-center text-lg-start my-3 my-md-0"><?php pll_e("Terms & Conditions"); ?></a>
-    </div>
+    <?php if (!empty($menu) && is_array($menu)) : ?>
+        <div class="d-flex flex-wrap flex-lg-nowrap d-flex-lg-inline justify-content-center">
+            <!-- Menu -->
+            <ul class="navbar-nav w-100 px-5 mx-auto flex-row flex-wrap justify-content-center" id="bottom-footer-menu">
+                <?php
+                foreach ($menu as $menu_item) :
+                    if (!$menu_item->menu_item_parent) :
+                ?>
+                        <li class="nav-item px-2">
+                            <a class="small text-white me-2 text-center text-lg-start my-3 my-md-0" href="<?php echo esc_url($menu_item->url); ?>">
+                                <?php echo esc_html($menu_item->title); ?>
+                            </a>
+                        </li>
+                <?php
+                    endif;
+                endforeach;
+                ?>
+            </ul>
+        </div>
+    <?php endif; ?>
 </div>
--- template-parts/footer/menu.php
+++ template-parts/footer/menu.php
@@ -1,33 +1,32 @@
-<?php 
-    /**
-     * Footer main menu
-     * @author FrankPonte
-     */
-    $menu_class = \SPANISHALACARTE\Inc\Menus::get_instance();
-    $menu_id = $menu_class->get_menu_id('salc-header-menu');
-    $menu = wp_get_nav_menu_items($menu_id);
+<?php
+
+/**
+ * Footer main menu
+ * @author FrankPonte
+ */
+$menu_class = \SPANISHALACARTE\Inc\Menus::get_instance();
+$menu_id = $menu_class->get_menu_id('salc-header-menu');
+$menu = wp_get_nav_menu_items($menu_id);
 ?>
 <div class="col-12">
     <nav class="my-5 d-flex align-items-center justify-content-center mb-4 align-items-center">
         <!-- Menu -->
-        <?php if(!empty($menu) && is_array($menu)):?>
+        <?php if (!empty($menu) && is_array($menu)) : ?>
             <ul class="navbar-nav w-100 w-lg-50 mx-auto flex-row flex-wrap justify-content-center" id="footer-menu">
-                <?php 
-            foreach ($menu as $menu_item):
-                if(!$menu_item->menu_item_parent):
-                    
-                    $child_menu_items = $menu_class->get_child_menu_items($menu, $menu_item->ID);
-                    ?>
+                <?php
+                foreach ($menu as $menu_item) :
+                    if (!$menu_item->menu_item_parent) :
+                ?>
                         <li class="nav-item px-xl-4 px-3">
-                            <a class="nav-link text-white text-center h6 text-decoration-underline" href="<?php echo esc_url($menu_item->url);?>">
-                                <?php echo esc_html( $menu_item->title); ?>
+                            <a class="nav-link text-white text-center h6 text-decoration-underline" href="<?php echo esc_url($menu_item->url); ?>">
+                                <?php echo esc_html($menu_item->title); ?>
                             </a>
                         </li>
-                        <?php
-                endif;
-            endforeach;
-            ?>
-        </ul>
+                <?php
+                    endif;
+                endforeach;
+                ?>
+            </ul>
         <?php endif; ?>
     </nav>
 </div>
