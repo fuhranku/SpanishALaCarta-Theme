c23f193bff415ff270727eba651570c61506381f
fuhranku <fponte@auction.com>
2023-04-23 18:29:39 -0400

    Now preloader is being loaded synchronously

--- assets/src/js/global.js
+++ assets/src/js/global.js
@@ -9,13 +9,3 @@ import {checkWPAdminBar} from './functions';
 $(window).on('scroll',function(){
     checkWPAdminBar();
 });
-
-$(window).on('load',function(){
-    // Hide preloader once the site has loaded
-    $('#site-preloader').addClass('page-loaded');
-    $('body').removeClass('overflow-hidden');
-    $('#site-preloader.page-loaded').one('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
-    function(e) {
-        $(this).addClass('d-none');
-    });
-});
--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -31,6 +31,7 @@ class Assets
     public function register_styles()
     {
         // Register Styles
+        wp_register_style('preloader-bundle', THEME_BUILD_CSS_URI . '/preloader.bundle.css', [], filemtime(THEME_BUILD_CSS_DIR_PATH . '/preloader.bundle.css'), 'all');
         wp_register_style('vendor-bundle-async', THEME_BUILD_CSS_URI . '/vendor.bundle.css', [], filemtime(THEME_BUILD_CSS_DIR_PATH . '/vendor.bundle.css'), 'all');
         wp_register_style('main-bundle-async', THEME_BUILD_CSS_URI . '/main.bundle.css', filemtime(THEME_BUILD_CSS_DIR_PATH . '/main.bundle.css'), 'all');
         wp_register_style(
@@ -107,6 +108,13 @@ class Assets
     {
         // Register Scripts
         wp_register_script(
+            'preloader-bundle',
+            THEME_BUILD_JS_URI . '/preloader.bundle.js',
+            ['jquery'],
+            '',
+            true
+        );
+        wp_register_script(
             'vendor-bundle-async',
             THEME_BUILD_JS_URI . '/vendor.bundle.js',
             ['jquery'],
@@ -169,6 +177,8 @@ class Assets
             filemtime(THEME_BUILD_JS_DIR_PATH . '/single_product.bundle.js'),
             true
         );
+
+        wp_enqueue_script('preloader-bundle');
         wp_enqueue_script('vendor-bundle-async');
         wp_enqueue_script('main-bundle-async');
 
